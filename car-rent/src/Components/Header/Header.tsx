c5edae3f8ed36c679d49dc164360e8ccb5f5b888
Aleksandr Dekov <sasha2005dekov@gmail.com>
2025-07-15 19:55:50 +0300

    reference post

---
+++ car-rent/src/Components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="ml-auto mr-auto bg-[url('./assets/bugatti.jpg')] bg-cover bg-no-repeat bg-center ">
+      <nav className="text-3xl min-h-140"></nav>
+    </div>
+  );
+}
