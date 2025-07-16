b5b7ef434b7d1d71ccf68363ff6d2efb27f3ac99
Aleksandr Dekov <sasha2005dekov@gmail.com>
2025-07-16 14:38:55 +0300

    settle up and add header

--- car-rent/src/App.jsx
+++ car-rent/src/App.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 
 function App() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+  });
+
   return (
-    <div className="ml-auto mr-auto bg-[url('./assets/bugatti.jpg')] bg-cover bg-no-repeat bg-center max-w-[80%] min-w-2xs">
-      <Header></Header>
+    <div className="bg-[url('/audi.jpg')]">
+      <Header scrolled={isScrolled}></Header>
+      <div className="h-1000"></div>
     </div>
   );
 }
--- car-rent/src/Components/Header/Header.tsx
+++ car-rent/src/Components/Header/Header.tsx
@@ -1,7 +1,45 @@
-export default function Header() {
+// import { ReactComponent as Logo } from "../../../public";
+
+export default function Header({ scrolled }) {
   return (
-    <div className="ml-auto mr-auto bg-[url('./assets/bugatti.jpg')] bg-cover bg-no-repeat bg-center ">
-      <nav className="text-3xl min-h-140"></nav>
-    </div>
+    <header
+      className={`flex content-between h-32 ${
+        scrolled
+          ? "bg-black/80 backdrop-blur-lg border-none"
+          : "bg-black/0 backdrop-blur-none border-b border-b-gray-600"
+      } fixed top-0 w-full 
+        transition-all duration-300 ease-in-out justify-between items-center`}
+    >
+      <nav className="flex justify-between align-middle">
+        <a href="#" className="ml-8 mr-8 text-[16px]">
+          models
+        </a>
+        <a href="#" className="ml-8 mr-8 text-[18px]">
+          cars for rent
+        </a>
+        <a href="#" className="ml-8 mr-8 text-[18px]">
+          rims
+        </a>
+        <a href="#" className="ml-8 mr-60 text-[18px]">
+          boutique
+        </a>
+      </nav>
+      <img
+        src="/logo-rent.svg"
+        alt="huy"
+        className="w-50 absolute left-[44%]"
+      />
+      <nav className="flex justify-between align-middle">
+        <a href="#" className="ml-8 mr-8 text-[18px]">
+          news
+        </a>
+        <a href="#" className="ml-8 mr-8 text-[18px]">
+          find a dealer
+        </a>
+        <a href="#" className="ml-8 mr-8 text-[18px]">
+          contact us
+        </a>
+      </nav>
+    </header>
   );
 }
